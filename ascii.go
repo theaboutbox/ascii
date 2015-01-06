9ed185a62304bda5de7a2fec6a70b9db21718123
Mat Ryer <thatmatryer@gmail.com>
2015-01-05 19:19:49 -0800

    added PrintBanner helper

--- ascii.go
+++ ascii.go
@@ -1,5 +1,11 @@
 package ascii
 
+import (
+	"fmt"
+
+	"io"
+)
+
 // GrayMeta is the ASCII for the main logo.
 const GrayMeta = `  _____ _____        __     ____  __ ______ _______       
  / ____|  __ \     /\\ \   / /  \/  |  ____|__   __|/\  TM
@@ -8,3 +14,12 @@ const GrayMeta = `  _____ _____        __     ____  __ ______ _______
 | |__| | | \ \  / ____ \| |  | |  | | |____   | |/ ____ \ 
  \_____|_|  \_\/_/    \_\_|  |_|  |_|______|  |_/_/    \_\
  `
+
+// PrintBanner prints a command line banner to w.
+func PrintBanner(w io.Writer, name string, version float32) {
+	w.Write([]byte(GrayMeta))
+	fmt.Fprintln(w)
+	fmt.Fprintf(w, "\t%s (v%g)\twww.graymeta.com", name, version)
+	fmt.Fprintln(w)
+	fmt.Fprintln(w)
+}
